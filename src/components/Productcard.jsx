ea077624649f08366072cd303749b7305190cf35
Bharath kumar <ipseudonerdi@gmail.com>
2025-04-23 19:00:22 +0530

    product-grid-fix

---
+++ src/components/Productcard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Productcard = ({ image, name, price, description, className = "" }) => {
+  return (
+    <div
+      class='pcard'
+      className={`bg-gray-100   border rounded-2xl shadow-md hover:shadow-2xl hover:scale-125 transform transition-all duration-500 ease-in-out 
+        hover:-translate-y-2 ${className} border-blue-300 mt-2 hover:opacity-80 hover:cursor-pointer`}
+    >
+      <div className='p-4'>
+        <img
+          src={image}
+          alt={name}
+          className='w-full h-48 object-cover rounded-xl mb-4'
+        />
+      </div>
+
+      <div className='bg-white w-full rounded-b-2xl p-2 flex flex-col justify-center items-center sm:items-start'>
+        <h2 className='text-lg font-bold'>{name}</h2>
+        <p className='text-gray-500 text-sm'>{description}</p>
+        <p className='text-blue-600 font-semibold mt-2'>{price}/-</p>
+      </div>
+    </div>
+  );
+};
+
+export default Productcard;
---
+++ src/components/ProductsGrid.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import Productcard from "./Productcard"; // adjust path if needed
+import product from "../assets/dp_hero.png";
+
+const ProductsGrid = () => {
+  return (
+    <>
+      <h1 className='text-7xl my-10 text-center font-semibold text-gray-800 '>
+        Products
+      </h1>
+      <section className='max-w-7xl mx-auto px-5 py-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+        <Productcard
+          image={product}
+          name='Product 1'
+          price='29.99'
+          description='This is a great product.'
+          className='col-span-1 '
+        />
+
+        <Productcard
+          image={product}
+          name='Product 2'
+          price='39.99'
+          description='Another awesome item.'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 3'
+          price='19.99'
+          description='Useful and affordable.'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 4'
+          price='49.99'
+          description='Top quality product.'
+        />
+        <Productcard
+          image={product}
+          name='Product 1'
+          price='29.99'
+          description='This is a great product.'
+          className='col-span-1'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 2'
+          price='39.99'
+          description='Another awesome item.'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 3'
+          price='19.99'
+          description='Useful and affordable.'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 4'
+          price='49.99'
+          description='Top quality product.'
+        />
+        <Productcard
+          image={product}
+          name='Product 1'
+          price='29.99'
+          description='This is a great product.'
+          className='col-span-1'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 2'
+          price='39.99'
+          description='Another awesome item.'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 3'
+          price='19.99'
+          description='Useful and affordable.'
+        />
+
+        <Productcard
+          image={product}
+          name='Product 4'
+          price='49.99'
+          description='Top quality product.'
+        />
+      </section>
+    </>
+  );
+};
+
+export default ProductsGrid;
