3d9ace38bbb37f7dba83287090d2e70df4dd9298
Bharath kumar <ipseudonerdi@gmail.com>
2025-04-23 23:11:20 +0530

    fixed-components-padding-mobiles

--- src/components/TestimonialGrid.jsx
+++ src/components/TestimonialGrid.jsx
@@ -5,7 +5,7 @@ const TestimonialGrid = () => {
   return (
     <>
       <h1 className='text-center text-6xl font-semibold mt-10'>Testimonials</h1>
-      <section className='max-w-7xl my-15 m-auto grid grid-cols-1 gap-5 sm:gap-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
+      <section className='max-w-7xl my-15 px-5 m-auto grid grid-cols-1 gap-5 sm:gap-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
         <TestimonialCard customer={"Bharath Kumar"} className='col-span-2' />
         <TestimonialCard customer={"Rithwik Kumar"} className='col-span-2' />
         <TestimonialCard customer={"Santosh"} className='col-span-2' />
