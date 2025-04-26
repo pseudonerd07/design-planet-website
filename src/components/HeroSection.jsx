545018687ef324082cfc1c1e612a7c720dbe099e
Your Name <karthiksriramoju11@gmail.com>
2025-04-27 00:46:15 +0530

    Button fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -15,12 +15,12 @@ const HeroSection = () => {
           </h1>
 
           <div className='mt-4 md:flex  md:justify-items-start md:items-center p-2 w-full'>
-            <div className='bg-blue-400 font-semibold py-2 px-7 text-center mt-5 w-full md:w-auto mx-0 rounded-sm md:rounded-4xl hover:bg-blue-500 hover:cursor-pointer transition-all mr-10 shadow-2xl hover:scale-150'>
+            <button className='bg-blue-400 font-semibold py-2 px-7 text-center mt-5 w-full md:w-auto mx-0 rounded-sm md:rounded-4xl hover:bg-blue-500 hover:cursor-pointer transition-all mr-10 shadow-2xl hover:scale-110'>
               Products
-            </div>
-            <div className='border border-blue-400 font-semibold text-white text-center mt-5 w-full md:w-auto mx-0 rounded-sm py-2 px-7 md:rounded-4xl hover:scale-105 hover:cursor-pointer transition-all duration-300 mr-10 shadow-2xl animate-pulse hover:animate-none'>
+            </button>
+            <button className='border border-blue-400 font-semibold text-white text-center mt-5 w-full md:w-auto mx-0 rounded-sm py-2 px-7 md:rounded-4xl hover:scale-110 hover:cursor-pointer transition-all duration-300 mr-10 shadow-2xl animate-pulse hover:animate-none'>
               Contact Us
-            </div>
+            </button>
           </div>
         </div>
 
