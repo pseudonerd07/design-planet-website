ed98c6529937fbd6e6f75c0eac9f65db3d0bd075
Bharath kumar <ipseudonerdi@gmail.com>
2025-04-28 00:40:04 +0530

    added navbar component to app-dot-jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import ProductsGrid from "./components/ProductsGrid";
 import FooterSection from "./components/FooterSection";
 import Testimonials from "./components/TestimonialCard";
 import TestimonialGrid from "./components/TestimonialGrid";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Navbar />
       <HeroSection />
 
       <ProductsGrid />
