8188b37d07fe48237b51f14d022b34754c3a225d
Bharath kumar <ipseudonerdi@gmail.com>
2025-04-28 01:22:24 +0530

    fixed testimonial cards in mobile

--- src/components/TestimonialGrid.jsx
+++ src/components/TestimonialGrid.jsx
@@ -10,13 +10,13 @@ const TestimonialGrid = () => {
       >
         Testimonials
       </h1>
-      <section className='max-w-7xl my-15 px-5 m-auto grid grid-cols-1 gap-5 sm:gap-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
-        <TestimonialCard customer={"Bharath Kumar"} className='col-span-2' />
-        <TestimonialCard customer={"Rithwik Kumar"} className='col-span-2' />
-        <TestimonialCard customer={"Santosh"} className='col-span-2' />
-        <TestimonialCard customer={"Srikanth"} className='col-span-2' />
-        <TestimonialCard customer={"Sai Krishna"} className='col-span-2' />
-        <TestimonialCard customer={"Pat Cummins"} className='col-span-2' />
+      <section className='max-w-7xl my-15 px-5 m-auto grid grid-cols-1 gap-5 sm:gap-14 md:grid-cols-2 lg:grid-cols-3'>
+        <TestimonialCard customer={"Bharath Kumar"} />
+        <TestimonialCard customer={"Rithwik Kumar"} />
+        <TestimonialCard customer={"Santosh"} />
+        <TestimonialCard customer={"Srikanth"} />
+        <TestimonialCard customer={"Sai Krishna"} />
+        <TestimonialCard customer={"Pat Cummins"} />
       </section>
     </>
   );
